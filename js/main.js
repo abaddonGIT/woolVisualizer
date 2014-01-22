c03df1a0e5ddac03b89efc0607b257bdabc93578
abaddonGIT <abaddongit@gmail.com>
2014-01-23 00:14:54 +0400

    Теперь можно любую песенку вставлять

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
+/******************************************************
+ * Copyright 2014 by Abaddon <abaddongit@gmail.com>
+ * @author Abaddon <abaddongit@gmail.com> 
+ * @version 0.0.1
+ * ***************************************************/
 var w = window,
     d = document,
     MP3_PATH = 'files/test.mp3',
-    MAX_PARTICLES = 110,
+    MAX_PARTICLES = 50,
     TWO_PI = Math.PI * 2,
     SMOOTHING = 0.3,
     FURIE = 512,
@@ -24,160 +29,177 @@ var w = window,
     COLORS = ['#69D2E7', '#A7DBD8', '#E0E4CC', '#F38630', '#FA6900', '#FF4E50', '#F9D423']; //цвета частиц
 
 
-var WoolAnalaser = function () {
-    var ctx = null,
+    var WoolAnalaser = function () {
+        "use strict";
+        var ctx = null,
         canva = null,
         config = null,
         particles = [],
-        audio = null;
+        audio = null,
+        input = d.querySelector('#song'),
         that = this;
 
 
-    config = this.config = {
-        fullscreen: true,
-        interval: 33,
-        type: "canvas"
-    };
-    /*
-    * Конструктор анализатора
-    */
-    var Analyse = function () {
-        var _that = this,
+        config = this.config = {
+            fullscreen: true,
+            interval: 10,
+            type: "canvas"
+        };
+        /*
+        * Конструктор анализатора
+        */
+        var Analyse = function () {
+            var _that = this,
             AudioContext = w.AudioContext || w.webkitAudioContext;
 
-        this.audio = new Audio();
-        this.audio.src = MP3_PATH;
-        this.audio.controls = true;
+            this.audio = new Audio();
+            this.audio.src = MP3_PATH;
+            this.audio.controls = true;
 
-        this.context = new AudioContext();
-        this.node = this.context.createJavaScriptNode(2048, 1, 1);
-        //Анализатор
-        this.analyser = this.context.createAnalyser();
-        this.analyser.smoothingTimeConstant = SMOOTHING;
-        this.analyser.fftSize = FURIE;
+            this.context = new AudioContext();
+            this.node = this.context.createJavaScriptNode(2048, 1, 1);
+            //Анализатор
+            this.analyser = this.context.createAnalyser();
+            this.analyser.smoothingTimeConstant = SMOOTHING;
+            this.analyser.fftSize = FURIE;
 
-        this.bands = new Uint8Array(this.analyser.frequencyBinCount);
+            this.bands = new Uint8Array(this.analyser.frequencyBinCount);
 
-        this.audio.addEventListener('canplay', function () {
-            _that.source = _that.context.createMediaElementSource(_that.audio);
-            _that.source.connect(_that.analyser);
-            _that.analyser.connect(_that.node);
-            _that.node.connect(_that.context.destination);
-            _that.source.connect(_that.context.destination);
+            this.audio.addEventListener('canplay', function () {
+                if (!_that.source) {
+                    _that.source = _that.context.createMediaElementSource(_that.audio);
+                    _that.source.connect(_that.analyser);
+                    _that.analyser.connect(_that.node);
+                    _that.node.connect(_that.context.destination);
+                    _that.source.connect(_that.context.destination);
 
-            _that.node.onaudioprocess = function () {
-                _that.analyser.getByteFrequencyData(_that.bands);
-                if (!_that.audio.paused) {
-                    //console.log(_that.bands);
-                    return typeof _that.update === "function" ? _that.update(_that.bands) : 0;
+                    _that.node.onaudioprocess = function () {
+                        _that.analyser.getByteFrequencyData(_that.bands);
+                        if (!_that.audio.paused) {
+                            //console.log(_that.bands);
+                            return typeof _that.update === "function" ? _that.update(_that.bands) : 0;
+                        }
+                    };
+                }
+            });
+            d.body.appendChild(_that.audio);
+
+            return this;
+        };
+
+        /*
+        * Кконструктор частиц
+        */
+        var Particle = function () {
+            this.init();
+        };
+
+        Particle.prototype = {
+            /*
+            * Создает частицу
+            */
+            init: function () {
+                this.x = that.random(canva.width);
+                this.y = that.random(canva.height);
+                this.level = 1 * that.random(4);
+                this.speed = that.random(SPEED.MIN, SPEED.MAX);
+                this.radius = that.random(RADIUS.MIN, RADIUS.MAX); //радиус частиц
+                this.color = that.random(COLORS); //цвет частицы
+                this.opacity = that.random(OPACITY.MIN, OPACITY.MAX);
+                this.band = Math.floor(that.random(128));
+            },
+            /*
+            * Рисует частицу в конве
+            */
+            draw: function () {
+                var pulsar, scale;
+                pulsar = Math.exp(this.pulse);
+                scale = pulsar * this.radius || this.radius;
+                ctx.save();
+                ctx.beginPath(); //Начинает отрисовку фигуры
+                ctx.arc(this.x, this.y, scale, 0, TWO_PI);
+                ctx.fillStyle = this.color; //цвет
+                ctx.globalAlpha = this.opacity / this.level; //прозрачность
+                ctx.closePath();
+                ctx.fill();
+                ctx.strokeStyle = this.color; //цвет рамки
+                ctx.stroke();
+                ctx.restore();
+
+                this.move();
+            },
+            /*
+            * Движение частицы
+            */
+            move: function () {
+                this.y -= this.speed * this.level;
+                //this.x += this.speed * this.level;
+                //Возврашам в начало частицы которые ушли за пределы хослста
+                if (this.y < -100) {
+                    this.y = canva.height;
+                }
+            }
+        }
+        /*
+        * Создает конву
+        */
+        this.init = function (contener) {
+            ctx = this.createCanvas(contener);
+            canva = ctx.canvas;
+            this.createParticles();
+        };
+        /*
+        * Создает частицы
+        */
+        this.createParticles = function () {
+            var particle = null, audio = null;
+
+            for (var i = 0; i < MAX_PARTICLES; i++) {
+                particle = new Particle();
+                particles.push(particle);
+            }
+            audio = new Analyse();
+
+            input.addEventListener('change', function () {
+                var song = this.value,
+                    fReader = new FileReader();
+
+                fReader.readAsDataURL(this.files[0]);
+                fReader.onloadend = function (event) {
+                    var e = event || w.event;
+                    audio.audio.src = e.target.result;
+                    audio.audio.load();
+                };
+            }, false);
+
+            audio.update = function (bands) {
+                var ln = MAX_PARTICLES;
+
+                while (ln--) {
+                    var loc = particles[ln];
+                    loc.pulse = bands[loc.band] / 256;
                 }
             };
-        });
-        d.body.appendChild(_that.audio);
-        
-        return this;    
-    };
-
-    /*
-    * Кконструктор частиц
-    */
-    var Particle = function () {
-        this.init();
-    };
-
-    Particle.prototype = {
-        /*
-        * Создает частицу
-        */
-        init: function () {
-            this.x = that.random(canva.width);
-            this.y = that.random(canva.height);
-            this.level = 1 * that.random(4);
-            this.speed = that.random(SPEED.MIN, SPEED.MAX);
-            this.radius = that.random(RADIUS.MIN, RADIUS.MAX); //радиус частиц
-            this.color = that.random(COLORS); //цвет частицы
-            this.opacity = that.random(OPACITY.MIN, OPACITY.MAX);
-            this.band = Math.floor(that.random(128));
-        },
-        /*
-        * Рисует частицу в конве
-        */
-        draw: function () {
-            var pulsar, scale;
-            pulsar = Math.exp(this.pulse);
-            scale = pulsar * this.radius || this.radius;
-            ctx.save();
-            ctx.beginPath(); //Начинает отрисовку фигуры
-            ctx.arc(this.x, this.y, scale, 0, TWO_PI);
-            ctx.fillStyle = this.color; //цвет
-            ctx.globalAlpha = this.opacity/this.level; //прозрачность
-            ctx.closePath();
-            ctx.fill();
-            ctx.strokeStyle = this.color;//цвет рамки
-            ctx.stroke(); 
-            ctx.restore();    
-        
-            this.move();
-        },
-        /*
-        * Движение частицы
-        */
-        move: function () {
-            this.y -= this.speed * this.level;
-            //this.x += this.speed * this.level;
-            //Возврашам в начало частицы которые ушли за пределы хослста
-            if (this.y < -100) {
-                this.y = canva.height;
-            } 
+            setInterval(that.action, this.config.interval);
         }
-    }
-    /*
-    * Создает конву
-    */
-    this.init = function (contener) {
-        ctx = this.createCanvas(contener);
-        canva = ctx.canvas;
-        this.createParticles();
-    };
-    /*
-    * Создает частицы
-    */
-    this.createParticles = function () {
-        var particle = null, audio = null;
-
-        for (var i = 0; i < MAX_PARTICLES; i++) {
-            particle = new Particle();
-            particles.push(particle);
-        }
-        audio = new Analyse();
-        audio.update = function(bands) {
+        /*
+        * Добавляет частицы в конву и анимирует их
+        */
+        this.action = function () {
             var ln = MAX_PARTICLES;
-
-            while (ln--) {
-                var loc = particles[ln];
-                loc.pulse = bands[loc.band]/256;
+            that.clear();
+            for (var i = 0; i < ln; i++) {
+                var loc = particles[i];
+                loc.draw();
             }
+        }
+        /*
+        * Чистит конву перед следующей анимацией
+        */
+        this.clear = function () {
+            ctx.clearRect(0, 0, canva.width, canva.height);
         };
-        setInterval(that.action, this.config.interval);
-    }
-    /*
-    * Добавляет частицы в конву и анимирует их
-    */
-    this.action = function () {
-        var ln = MAX_PARTICLES;
-        that.clear();
-        for (var i = 0; i < ln; i++) {
-            var loc = particles[i];
-            loc.draw();
-        } 
-    }
-    /*
-    * Чистит конву перед следующей анимацией
-    */
-    this.clear = function () {
-        ctx.clearRect(0, 0, canva.width, canva.height);
     };
-};
 
 WoolAnalaser.prototype = {
     /*
